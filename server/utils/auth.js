dc95bb17e7874e6c0556bd479727c41bc602185c
davidjaguilar104 <davidjaguilar104@gmai.com>
2022-02-23 11:52:14 -0600

    changed auth.js payload to use username/email/id

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -26,8 +26,8 @@ module.exports = {
 
     return req;
   },
-  signToken: function ({ firstName, email, _id }) {
-    const payload = { firstName, email, _id };
+  signToken: function ({ username, email, id }) {
+    const payload = { username, email, id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
