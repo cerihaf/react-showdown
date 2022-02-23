a0a4015f535406a4f1b0e069bdee6f454599fd40
Ceri Trammell <cerihaf@gmail.com>
2022-02-23 16:25:25 -0600

    set up beginning stages of the votebox component and created a button.js file within it to render the buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import VoteBox from "../components/Vote";
 
 const Home = () => {
   
   return (
     <div className="container">
       <h1 className="underline">Hello World</h1>
+      <VoteBox/>
     </div>
   );
 };
