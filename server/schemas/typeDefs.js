c31cb5895717ac79fa9a68d288a994d3058ec3ce
Ceri Trammell <cerihaf@gmail.com>
2022-02-22 20:23:53 -0600

    initial file set up

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+
+  type User {
+ 
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+   
+  }
+
+  type Mutation {
+
+  }
+`;
+
+module.exports = typeDefs;
