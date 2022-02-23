fdd081c5c646ccf649d0d1a0a055b6625d04c016
davidjaguilar104 <davidjaguilar104@gmai.com>
2022-02-23 11:38:04 -0600

    addUser and login mutation functioning with MySQL

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,44 @@
-// const { AuthenticationError } = require("apollo-server-express");
-// const { User, Product, Category, Order } = require("../models");
-// const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Comment, Matchup, Vote } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     helloWorld: () => {
       return "Hello World";
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create({
+        username: args.username,
+        email: args.email,
+        password: args.password,
+      });
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect Credentials");
+      }
+
+      const correctPw = user.checkPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect Credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,24 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type User {
+    id: ID
+    username: String
+    email: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
   type Query {
     helloWorld: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
