10d775f8dd3e129393c1fc92a87bb03b5132785d
Gwack <gracalynntrammell@gmail.com>
2022-02-25 11:27:00 -0600

    created footer and added to App.js

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="bg-persiangreen">
+            <div>
+                <a href="https://github.com/cerihaf/react-showdown">Grace, Ceri, George, Grant, and David</a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
