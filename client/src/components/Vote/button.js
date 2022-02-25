f20f9f38c6e9d919973c337f829c85e027563401
Gwack <gracalynntrammell@gmail.com>
2022-02-25 14:03:28 -0600

    added styling to animal buttons

--- client/src/components/Vote/button.js
+++ client/src/components/Vote/button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const VoteButton = ({animal, bgcolor}) => {
 
-    const classnames = `w-full border-solid border-2 border-amber-800 ${bgcolor} h-96 text-4xl rounded-3xl`
+    const classnames = `w-10/12 p-6 h-60 border-solid border-2 border-amber-800 ${bgcolor} text-4xl rounded-3xl`
 
 
     return(
