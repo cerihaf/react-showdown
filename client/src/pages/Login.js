929874b3741984eef3c42cc756e8a56d8fbdbf67
Ceri Trammell <cerihaf@gmail.com>
2022-02-26 16:36:46 -0600

    committing to pull down for develop

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,7 +5,9 @@ const Login = () => {
   return (
     <section className="flex flex-1">
       <div className="flex w-full flex-row lg:flex-row">
-        <div className="w-full lg:w-1/2 lion"></div>
+        <div className="w-full lg:w-1/2 lion">
+          <h1>Who's your champion?</h1>
+        </div>
         <div className="w-full lg:w-1/2 flex justify-center items-center flex-1">
           <div>
             <LoginForm />
