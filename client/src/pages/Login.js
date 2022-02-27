2e9df05db3a384ce0b97c3a60df12a6013b92cc3
Ceri Trammell <cerihaf@gmail.com>
2022-02-26 18:08:51 -0600

    added text animation to the login page above the lions head

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,8 +5,26 @@ const Login = () => {
   return (
     <section className="flex flex-1">
       <div className="flex w-full flex-row lg:flex-row">
-        <div className="w-full lg:w-1/2 lion">
-          <h1>Who's your champion?</h1>
+        <div className="w-full lg:w-1/2">
+          <svg viewBox="0 0 960 300" className="svg pb-8">
+            <symbol id="s-text">
+              <text textAnchor="middle" x="50%" y="80%">
+                Let's Fight{" "}
+              </text>
+              <text textAnchor="middle" x="51%" y="80%">
+                Let's Fight{" "}
+              </text>
+            </symbol>
+
+            <g className="g-ants">
+              <use xlinkHref="#s-text" className="text-copy"></use>
+              <use xlinkHref="#s-text" className="text-copy"></use>
+              <use xlinkHref="#s-text" className="text-copy"></use>
+              <use xlinkHref="#s-text" className="text-copy"></use>
+              <use xlinkHref="#s-text" className="text-copy"></use>
+            </g>
+          </svg>
+          <img src="./images/lion.png" alt="lion looking up into nothingness" className="pt-2"/>
         </div>
         <div className="w-full lg:w-1/2 flex justify-center items-center flex-1">
           <div>
