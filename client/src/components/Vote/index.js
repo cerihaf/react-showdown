cd063ce2981bb9b74589a057b8958e561011266c
Ceri Trammell <cerihaf@gmail.com>
2022-02-26 23:22:36 -0600

    refactored the getNextMatchup function and get_matchup query into the home.js

--- client/src/components/Vote/index.js
+++ client/src/components/Vote/index.js
@@ -1,46 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import VoteButton from "./button";
-import { useQuery } from "@apollo/client";
-import { GET_MATCHUP } from "../../utils/queries";
 import NextMatchupBtn from "./NextMatchupBtn";
 
-
-
-const VoteBox = ({hasVoted, setHasVoted}) => {
-  const [matchupId, setMatchupId] = useState(1)
-  const { loading, data } = useQuery(GET_MATCHUP, {
-  variables: { id: matchupId },
-  });
-
-  const matchup = data?.getMatchup || {};
-
-  function handleNextMatchup() {
-    if (matchupId === 13 ) {
-      setMatchupId(1)
-    } else {
-      setMatchupId((prevState) => prevState + 1 )
-    }
-    setHasVoted(false)
-    //still need to save hasVoted to DB
-  }
-
-if (loading) {
-  return <div>Loading...</div>;
-} 
-
+const VoteBox = ({ hasVoted, setHasVoted, matchup, handleNextMatchup }) => {
   return (
     <section>
       <div className="bg-charcoal flex justify-between items-center max-w-5xl h-60 mx-auto">
-        <VoteButton animal={matchup.animal_1} bgcolor="bg-burntsienna" setHasVoted={setHasVoted} />
+        <VoteButton
+          animal={matchup.animal_1}
+          bgcolor="bg-burntsienna"
+          setHasVoted={setHasVoted}
+        />
         <span className="leading-none text-maizecrayola">OR</span>
-        <VoteButton animal={matchup.animal_2} bgcolor="bg-persiangreen" setHasVoted={setHasVoted} /> 
+        <VoteButton
+          animal={matchup.animal_2}
+          bgcolor="bg-persiangreen"
+          setHasVoted={setHasVoted}
+        />
+      </div>
+      {hasVoted && (
+        <div className="bg-charcoal flex justify-between items-center w-36 h-24 mx-auto">
+          <NextMatchupBtn handleNextMatchup={handleNextMatchup} />
         </div>
-      {  hasVoted  &&  (<div className="bg-charcoal flex justify-between items-center w-36 h-24 mx-auto">
-      <NextMatchupBtn handleNextMatchup={handleNextMatchup} />
-      </div>)} 
-      
-  </section>
-
+      )}
+    </section>
   );
 };
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,42 @@
 import React, { useState } from "react";
 import VoteBox from "../components/Vote";
 import PieChart from "../components/Chart";
+import { GET_MATCHUP } from "../utils/queries";
+import { useQuery } from "@apollo/client";
 
 const Home = () => {
   const [hasVoted, setHasVoted] = useState(false);
+  const [matchupId, setMatchupId] = useState(1);
+
+  const { loading, data } = useQuery(GET_MATCHUP, {
+    variables: { id: matchupId },
+  });
+
+  const matchup = data?.getMatchup || {};
+
+  function handleNextMatchup() {
+    if (matchupId === 13) {
+      setMatchupId(1);
+    } else {
+      setMatchupId((prevState) => prevState + 1);
+    }
+    setHasVoted(false);
+    //still need to save hasVoted to DB
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="container">
-      <VoteBox setHasVoted={setHasVoted} hasVoted={hasVoted} />
-      {hasVoted && <PieChart />}
+      <VoteBox
+        matchup={matchup}
+        handleNextMatchup={handleNextMatchup}
+        setHasVoted={setHasVoted}
+        hasVoted={hasVoted}
+      />
+      {hasVoted && <PieChart data={matchup} />}
     </div>
   );
 };
