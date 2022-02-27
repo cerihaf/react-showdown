6133616c46cb024a4b7c1c612f407d03ab18db46
Ceri Trammell <cerihaf@gmail.com>
2022-02-27 12:10:49 -0600

    resized the chart, ready for comment.js to render next to it

--- client/src/components/Vote/NextMatchupBtn.js
+++ client/src/components/Vote/NextMatchupBtn.js
@@ -4,7 +4,7 @@ function NextMatchupBtn({handleNextMatchup}) {
   
     return(
         <div className="bg-maizecrayola hover:bg-sandybrown hover:text-maizecrayola text-zinc-600 flex-1 flex justify-center border-solid border-2 border-HeliotropeGray text-lg rounded-md">
-            <button onClick={handleNextMatchup} className="font-semibold" ><p className="">Next Match</p></button>
+            <button onClick={handleNextMatchup} className="font-semibold" ><p className="font-brand">Next Match</p></button>
         </div>
     )
 }
