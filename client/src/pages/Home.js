f184c858ad371de2898133b05afd57b5e8991001
Ceri Trammell <cerihaf@gmail.com>
2022-02-26 18:24:28 -0600

    changed routes for logout to redirect you back to the login page and got rid of login and signup buttons becuase the site automatically directs you to the login page unless already logged in.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,15 +2,12 @@ import React, { useState } from "react";
 import VoteBox from "../components/Vote";
 
 const Home = () => {
-  
-  const [hasVoted, setHasVoted] = useState(false)
-
+  const [hasVoted, setHasVoted] = useState(false);
 
   return (
     <div className="container">
-      <h1 className="underline">Hello World</h1>
       <VoteBox setHasVoted={setHasVoted} hasVoted={hasVoted} />
-      {hasVoted  &&  (<h1>Render comments/chart here</h1>)}
+      {hasVoted && <h1>Render comments/chart here</h1>}
     </div>
   );
 };
