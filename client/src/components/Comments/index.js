5f4e340f0aca6acfaa871cee39baab2757a46c3d
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2022-02-26 18:50:20 -0600

    got chart to render on home page

--- client/src/components/Comments/index.js
+++ client/src/components/Comments/index.js
@@ -1,35 +1,9 @@
 import React, { useState } from "react";
-import comment from "../../../../server/schemas/resolvers";
 
 const Comments = () => {
   const [hasVoted, setHasVoted] = useState(false);
 
-  return (
-    <div classNameName="bg-charcoal">
-      <div className="comments">
-        <div className="comment-title">
-          See what others said about this Matchup
-        </div>
-        <article className="comment-container comment-color-{{comment.color}}">
-          <img
-            className="user-avi"
-            height="50"
-            width="50"
-            src="https://avatars.dicebear.com/api/croodles/{{comment.username}}.svg"
-            alt="{{comment.username}}"
-          />
-          <div className="comment-body">
-            <div className="author">
-              <h4>{comment.username}</h4>
-            </div>
-            <div className="comment">{comment.comment}</div>
-          </div>
-        </article>
-        <textarea placeholder="Voice your opinion!"></textarea>
-        <button className="submit-btn">Submit</button>
-      </div>
-    </div>
-  );
+  return <div className="bg-charcoal">placeholder text</div>;
 };
 
 export default Comments;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import VoteBox from "../components/Vote";
+import PieChart from "../components/Chart";
 
 const Home = () => {
-  
-  const [hasVoted, setHasVoted] = useState(false)
-
+  const [hasVoted, setHasVoted] = useState(false);
 
   return (
     <div className="container">
       <h1 className="underline">Hello World</h1>
       <VoteBox setHasVoted={setHasVoted} hasVoted={hasVoted} />
-      {hasVoted  &&  (<h1>Render comments/chart here</h1>)}
+      {hasVoted && <PieChart />}
     </div>
   );
 };
